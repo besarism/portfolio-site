609d0040fe81af15e55be17898f906aa06975988
Besar <b3sar11@gmail.com>
2018-12-24 14:22:23 +0100

    webapp add

--- webapp/urls.py
+++ webapp/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
+from . import views
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^webapp/', include('webapp.urls'))
+    url(r'^$', views.index, name='index'),
 ]
