9369f21b92efe8222d1e16075b9fa3e54545c37a
Besar <b3sar11@gmail.com>
2018-12-26 09:12:00 +0100

    Pasing variables from python to html

--- personal/views.py
+++ personal/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'personal/home.html')
 
+def contact(request):
+    content = {'content': ['Checkout my github handle','github.com/besarism']}
+    return render(request, 'personal/contact.html', content)
+
